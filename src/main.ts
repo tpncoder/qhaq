4bb01282c7f201c0a67e683b632cf47467bf9de1
tpncoder <tejasprotim@gmail.com>
2023-03-29 16:41:17 +0530

    (fix) broken deps

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/main.css";
-import VueCodeHighlight from 'vue-code-highlight';
 
 const app = createApp(App);
 
 app.use(router);
-app.use(VueCodeHighlight)
 
 app.mount("#app");

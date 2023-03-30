38d4a783a57048b7537e2d2ec652d4ab9ab99a0d
tpncoder <tejasprotim@gmail.com>
2023-03-30 19:23:52 +0530

    (feat) better ui for mobile

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -49,8 +49,9 @@
 
 		if (!data) return;
 
-		const filtData = data.filter(datum => datum.title.toLowerCase().trim().includes(title.toLowerCase().trim()));
-		console.log(filtData)
+		const filtData = data.filter(datum => { 
+			datum.title.toLowerCase().trim().includes(title.toLowerCase().trim())
+		});
 
 		if (title.trim().length !== 0 && data.length > 0) {
 			articles.value = filtData;
@@ -82,12 +83,16 @@
 	<div class="dyk" style="overflow: auto">
 		<div class="quote" style="position: relative;">
 			<h2>
-				"Don't just search for answers – find them with QBlog, the ultimate source of trusted information!"
+				"Don't just search for answers – find them with QBlog, 
+				the ultimate source of trusted information!"
 			</h2>
 		</div>
 		<div class="message" style="position: relative;">
 			<h2>Achieve technological enlightenment with Q-Blog</h2>
-			<p style="font-size: 1.5em">Did you know that we have advanced a lot in tech? You can use our resources to know about these advancements.</p>
+			<p style="font-size: 1.5em">
+				Did you know that we have advanced a lot in tech? 
+				You can use our resources to know about these advancements.
+			</p>
 		</div>
 		<img 
 			src="/buddha.png" 
@@ -101,14 +106,23 @@
 	<h1 style="text-align: center;">Articles</h1>
 	
 	<div class="filters">
-		<div v-for="(filt, index) in filters" :key="index">
-			<button @click="() => { filter=filt.toLowerCase() }">{{ filt }}</button>
+		<div class="buttons">
+			<div v-for="(filt, index) in filters" :key="index">
+				<button @click="() => { filter=filt.toLowerCase() }">{{ filt }}</button>
+			</div>
 		</div>
 	</div>
 	<br>
 
 	<div class="container">
-		<input v-model="sFilter" class="search" @keyup.enter.prevent='() => { articleSearch(sFilter.toLowerCase()) }' placeholder="Seach for posts...">
+		<input 
+			v-model="sFilter" 
+			class="search" 
+			@keyup.enter.prevent='() => { 
+				articleSearch(sFilter.toLowerCase()) 
+			}' 
+			placeholder="Seach for posts..."
+		>
   	<div v-if="loaded" class="articles">
     	<div v-for="item in articles" :key="item.title">
       	<div class="card">
@@ -116,17 +130,22 @@
           	<RouterLink :to="'article' + '/' + item.title" class="title">{{ item.title }}</RouterLink>
         	</h2>
 					<p class="summary">{{ item.summary }}</p>
-          <RouterLink :to="'article' + '/' + item.title" class="title" style="text-decoration: underline;">Read More</RouterLink>
-      	</div>
-    	</div>
-  	</div>
+					<div id="read-m">
+						<RouterLink 
+							:to="'article' + '/' + item.title" 
+							class="rm" 
+							style="text-decoration: underline;"
+						>Read More</RouterLink>
+      		</div>
+    		</div>
+  		</div>
+		</div>
 	</div>
 </template>
 
 <style scoped>
 .articles{
 	position: relative;
-
 }
 
 h3, h2{
@@ -144,9 +163,21 @@ h3, h2{
 	margin-left: -35%;
 	text-align: justify;
 	overflow: hidden;
-white-space: nowrap;
+	white-space: nowrap;
 	text-overflow: ellipsis;	
 }
+
+/* select{
+  padding: 0.6rem;  
+  margin-bottom: 3vh;
+  background-color: var(--card-bg-color);
+  color: var(--font-color);
+  border: 1px solid var(--border-color);
+  width: 100%;
+  text-align: center;
+  position: relative;
+  font-family: monospace ;
+} */
 
 .card{
   background-color: var(--card-bg-color);
@@ -154,7 +185,7 @@ white-space: nowrap;
   margin-bottom: 1.3em;
   border: 1px solid var(--border-color);
   border-style: solid;
-	height: 13.5em;
+	min-height: 13.5em;
 	text-align: center;
 	width: 85%;
 	position: relative;
@@ -172,17 +203,16 @@ white-space: nowrap;
 .title, p{
 	font-size: 1.3em;
 }
+
 input{
 	background-color: var(--card-bg-color);
 	color: var(--font-color);
 	border: 1px solid var(--border-color);
 	padding: 1em;
 	margin-bottom: 1em;
-	display: block;
   margin : 1.5em auto;
-	width: 85%
+	width: 100%;
 }
-
 
 button{
 	margin-right: 0.5em;
@@ -232,7 +262,6 @@ button:hover{
 	width: 27em;
 	font-size: 13px;
 	display: inline-block;
-	align-self: stretch;
 	display: flex;
 }
 
@@ -265,6 +294,11 @@ button:hover{
 	color: var(--border-color);
 }
 
+.rm{
+	font-size: 1.2em;
+	margin-bottom: 1.2em;
+}
+
 ::-webkit-input-placeholder { 
 	font-family: "Inria Serif";
 	text-align: left;
@@ -287,8 +321,23 @@ button:hover{
 }
 
 @media ( max-width: 800px ) {
-	.filters{
-		display: none
+	.filters > .buttons{
+		display: none;
+	}
+	
+	input {
+		width: 70%;
+	}
+
+	.rm {
+		margin-bottom: 2em;
+		padding-bottom: 2em;
+	}
+}
+
+@media ( min-width: 800px ) {
+	.filters > .selectFilter {
+		display: none;
 	}
 }
 
@@ -305,5 +354,14 @@ button:hover{
 	margin-left: -35%;
 	height: 100%;
 	padding: 0;
+	display: flex;
+	flex-direction: column;
+}
+
+h2{
+	width: 70%;
+	position: relative;
+	left: 50%;
+	margin-left: -35%;
 }
 </style>
